2518422ce8da0381881296577eae8876efa8e473
Vladislav Ufimtsev <vladislavufimtsev@MacBook-Air-Vladislav-7836.local>
2023-11-02 22:15:10 +0300

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState();
+
+  const getQuotes = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/quotes");
+      setQuotes(response.data.quotes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const generateRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setRandomQuote(quotes[randomIndex]);
+    console.log(quotes[randomIndex]);
+  };
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:p-32 px-10 flex items-center justify-center flex-col gap-y-20 bg-black w-full h-[100vh] font-sans">
+      {randomQuote && (
+        <article className="bg-slate-200 shadow-md shadow-slate-50 md:w-auto w-full-10 h-auto rounded-xl p-10 text-black">
+          <p className="flex items-start md:flex-row flex-col gap-x-2 bg-slate-400 p-2 rounded-md shadow-md font-medium mb-3">
+            <ImQuotesLeft />
+            {randomQuote.quote}
+            <ImQuotesRight />
+          </p>
+          <p className="text-gray-500 font-bold">{randomQuote.author}</p>
+        </article>
+      )}
+      <button
+        onClick={generateRandomQuote}
+        className="text-white border border-white  p-2 rounded-lg hover:text-black hover:bg-white transition ease-in-out duration-75"
+      >
+        Generate Quote
+      </button>
     </div>
   );
 }
